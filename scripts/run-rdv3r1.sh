32d3552160bf4b5197fa318369b7f071798eabc0
“jiahao.song” <2651142905@qq.com>
2026-09-09 10:25:49 +0800

    feat: add interactive RD-V3-R1 run mode

--- scripts/run-rdv3r1.sh
+++ scripts/run-rdv3r1.sh
@@ -6,5 +6,18 @@ root=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 model=$(find "$root/models/rdv3r1/models" -type f -name FVP_RD_V3_R1 -executable -print -quit)
 test -n "$model"
 
+case ${1:-} in
+    '') interactive=false ;;
+    --interactive) interactive=true ;;
+    *)
+        printf 'Usage: %s [--interactive]\n' "${0##*/}" >&2
+        exit 2
+        ;;
+esac
+
 cd "$root/stack/model-scripts/rdinfra"
-MODEL="$model" ./boot-buildroot.sh -p rdv3r1 -j -t
+if [[ $interactive == true ]]; then
+    MODEL="$model" ./boot-buildroot.sh -p rdv3r1
+else
+    MODEL="$model" ./boot-buildroot.sh -p rdv3r1 -j -t
+fi
